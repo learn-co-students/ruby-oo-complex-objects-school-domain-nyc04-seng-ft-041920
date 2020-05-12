a586fc66f3c5a239b3107f60d97e9dacb23b3a25
github username <github email address>
2020-05-12 15:37:55 -0400

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,29 @@
+require 'pry' 
 # code here!
+
+class School
+
+    def initialize(school)
+        @school = school
+    end
+
+    def roster
+        @roster = {}
+    end
+
+    def add_student(name, grade)
+        @name = name
+        @grade = grade
+
+        binding.pry
+
+        roster[@grade] = []
+        roster[@grade] << @name
+        # if roster[:grade] 
+        #     # roster[:grade] << name
+        # else
+        #     roster[:grade] = []
+        #     roster[:grade] << [:name]
+        # end
+    end
+end

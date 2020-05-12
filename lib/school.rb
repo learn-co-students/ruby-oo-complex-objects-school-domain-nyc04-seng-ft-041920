1884d6607669c12fa70c307a540fc2c6ee70c61c
github username <github email address>
2020-05-12 16:04:21 -0400

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -2,28 +2,32 @@ require 'pry'
 # code here!
 
 class School
-
+    attr_writer :roster
     def initialize(school)
         @school = school
+        @roster = {}
     end
 
     def roster
-        @roster = {}
+        @roster
     end
 
     def add_student(name, grade)
         @name = name
         @grade = grade
 
-        binding.pry
+        @roster[grade] ? @roster[grade] << name : @roster[grade] = [name]
+    end
 
-        roster[@grade] = []
-        roster[@grade] << @name
-        # if roster[:grade] 
-        #     # roster[:grade] << name
-        # else
-        #     roster[:grade] = []
-        #     roster[:grade] << [:name]
-        # end
+    def grade(grade)
+        @roster[grade]
+    end
+
+    def sort
+        new_roster = {}
+        @roster.each_pair do |key, value| 
+            new_roster[key] = value.sort
+        end
+        new_roster
     end
 end

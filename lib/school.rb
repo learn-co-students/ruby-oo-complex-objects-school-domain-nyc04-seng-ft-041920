51d9376ea33dd6277bac3ac671f2a3b2683296ae
hkofkin <hkofkin@gmail.com>
2020-05-13 00:50:54 -0400

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -10,12 +10,13 @@ class School
     end
 
     def add_student(name, grade)
-        if @roster[grade]
-            @roster[grade] << name
-        else
-            @roster[grade] = []
-            @roster[grade] << name
-        end
+        @roster[grade] << name ||= []
+        # if @roster[grade]
+        #     @roster[grade] << name
+        # else
+        #     @roster[grade] = []
+        #     @roster[grade] << name
+        # end
     end
 
     def grade(num)

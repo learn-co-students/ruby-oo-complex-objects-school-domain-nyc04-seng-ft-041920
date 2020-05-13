cf842843142af3a81502bb16d3b9a388ba273e96
j3ffh95 <j3ffh95@gmail.com>
2020-05-12 23:45:16 -0400

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,32 @@
+# frozen_string_literal: true
+
+require 'pry'
 # code here!
+class School
+  attr_accessor :name, :roster
+
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+  def add_student(name, grade)
+    if @roster[grade]
+      @roster[grade].push name
+    else
+      @roster[grade] = [name]
+    end
+  end
+
+  def grade(grade_num)
+    @roster[grade_num]
+  end
+
+  def sort
+    sorted_hash = {}
+    @roster.each do |key, val|
+      sorted_hash[key] = val.sort
+    end
+    sorted_hash
+  end
+end

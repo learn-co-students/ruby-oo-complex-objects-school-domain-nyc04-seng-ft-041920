6863a3e9eb0dc8fe6c967a06ad52b4116696116b
stephepush <7speter@gmail.com>
2020-05-16 21:12:19 -0400

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,34 @@
 # code here!
+
+#import "pry"
+
+class School
+
+    attr_accessor :roster
+
+    def initialize(name)
+        @name = name
+        @roster = {}
+        
+    end
+
+   def add_student(student, grade)
+        if @roster.has_key?(grade)
+            #roster[grade] << student
+            roster[grade].push(student)
+        else 
+            roster[grade] = []
+            #roster[grade] << student 
+            roster[grade].push(student)
+        end
+   end
+
+   def grade(grade)
+    @roster[grade]
+   end
+
+   def sort
+    @roster.each { |key, array| array.sort_by! { |s| [s[0]] }}
+   end
+
+end
